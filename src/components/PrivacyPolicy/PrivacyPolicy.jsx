4d16b56067ce597a5caae284e17a487dc82c327c
Arkip <archippemuhayiri@gmail.com>
2026-01-20 23:17:01 +0300

    correcting loading images

--- src/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import logo from "../../assets/images/agroflex.png";
 import { Link } from "react-router-dom";
-import BlurImage from "../BlurImage/BlurImage";
 
 const PrivacyPolicy = () => {
   return (
     <div className="privacy-policy-container relative">
-      <BlurImage
+      <img
         src={logo}
         alt="Agroflex Logo"
-        className="privacy-policy-logo badge flex  opacity-7 justify-self-center w-[30rem] md:w-[40rem] lg:w-[46rem] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  -z-1  "
+        className="privacy-policy-logo badge opacity-7 justify-self-center w-[30rem] md:w-[40rem] lg:w-[46rem] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  "
       />
       <header className="privacy-policy-header">
         <h1>Politique de Confidentialité pour Agroflex</h1>
