6c89827e1a3678105a822d5becb650628c25ee6e
Arkip <archippemuhayiri@gmail.com>
2026-01-28 20:34:12 +0300

    chat ai conversation

--- src/components/ChatAi/ChatAi.jsx
+++ src/components/ChatAi/ChatAi.jsx
@@ -4,16 +4,20 @@ import chatai_1 from "../../assets/images/ai/ai_res_1.png";
 function ChatAi() {
   return (
     <section className="w-full h-[60vh] flex justify-center items-center">
-      <h2 className="text-3xl font-bold">Chat AI Component</h2>
+      <h2 className="font-semibold pt-4  pb-10 px-5 text-center text-slate-600 text-2xl lg:text-3xl">
+        Chat AI Component
+      </h2>
+      <div className="w-full h-[60vh] bg-gradient-to-r from-green-50 to-green-100 p-10 flex flex-col gap-5 justify-center items-center">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam,
+          nesciunt molestiae unde nemo repudiandae laborum nam aut expedita
+          porro eius rem, ab quo saepe culpa error itaque fuga! Fuga aut hic,
+          veritatis doloribus sint quod quas et alias exercitationem illum ipsa
+          itaque nulla facilis ea? Maiores id molestias quaerat ducimus?
+        </p>
+      </div>
       <div>
-        {/* Chat AI functionality will be implemented here */}
-        Chat AI coming soon!
-        <img
-          height={90}
-          width={160}
-          src={chatai_1}
-          alt="Ai assistant conversation"
-        />
+        <img src={chatai_1} alt="chat ai response example" />
       </div>
     </section>
   );
